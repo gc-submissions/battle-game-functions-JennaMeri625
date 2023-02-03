fdd2cc97dc54741a91e71842fc8a83ff495ed3a5
JennaMeri625 <114081695+JennaMeri625@users.noreply.github.com>
2023-02-03 17:22:39 -0500

    0 failures!

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 // TODO - write your code here.
 
@@ -7,7 +7,7 @@ const randomDamage = () => Math.floor(Math.random() * 10 ) + 1;
                                                 
 let randNum = null;
 
-const chooseOption = (opt1, opt2) => randNum = (Math.random() > 0.5 ? 1 : 0) == 0 ? opt1 : opt2;
+let chooseOption = (opt1, opt2) => randNum = (Math.random() > 0.5 ? 1 : 0) == 0 ? opt1 : opt2;
 
 function attackPlayer(health) {
     return Number(health) - randomDamage();
@@ -24,23 +24,24 @@ let attacker = null;
 function fight(player1, player2, player1Health, player2Health){
     while(true) 
     {
-        attacker = chooseOption(player1, player2);
-            if (attacker = player1) {
+        attacker = chooseOption(player1, player2);  
+
+        if (attacker === player1) {
                 player2Health = attackPlayer(player2Health);
-                console.log(logHealth(player2, player2Health));
+               logHealth(player2, player2Health);
                 if (isDead(player2Health)) {
-                    console.log(logDeath(player1, player2));
+                    logDeath(player1, player2);
                     break;
                 }
             }
             else {
                 player1Health = attackPlayer(player1Health);
-                console.log(logHealth(player1, player1Health));
+                logHealth(player1, player1Health);
                 if (isDead(player1Health)) {
-                    console.log(logDeath(player2, player1));
+                    logDeath(player2, player1);
                     break;
                 }
                 };
     }
 }
-console.log(fight(`Mitch`, `Adam`, 100, 100))
+fight("Mitch", "Adam", 100, 100)
